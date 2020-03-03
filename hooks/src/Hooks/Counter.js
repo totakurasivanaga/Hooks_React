ae0e67122474466583f8ebdfb109805cae5d2c9e
totakurasivanaga <totakurasivanaga@gmail.com>
2020-03-03 23:01:05 +0530

    updating usestate code

---
+++ hooks/src/Hooks/Counter.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react';
+ 
+const Counter = ()  => {
+
+    //Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+             <p>You clicked {count} times</p>
+           <button onClick = {() => setCount(count + 1)}>Click Me</button>
+        </div>
+    );
+}
+ 
+export default Counter;
