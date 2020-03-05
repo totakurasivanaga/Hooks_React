5010bde0fdb616660fb610657c4209c51ddf3560
totakurasivanaga <totakurasivanaga@gmail.com>
2020-03-05 20:32:42 +0530

    updating usestate array minor codechang

--- hooks/src/Hooks/CounterFour.js
+++ hooks/src/Hooks/CounterFour.js
@@ -7,18 +7,20 @@ function CounterFour() {
   const [items, setItems] = useState([]);
 
   const addItem =() => {
-    setItems([...items], {
+    setItems([...items, {
         id: items.length,
-        value: Math.floor(Math.random() * 10) + 1
-    })
+        value: Math.floor((Math.random() * 10) + 1)
+        // value: 10
+    }])
   }
   return (
     <div>
         <button onClick = {addItem}>Add a Number</button>
         <ul>
-           {items.map(item => (<li key = {items.id}>{items.value}</li>))}
+           {items.map(item => (
+            <li key = {items.id}>{items.value}</li>
+           ))}
         </ul>
-      
     </div>
   );
 }
