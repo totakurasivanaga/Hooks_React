f1624bc6974cd7d3de2d8d81439af2cdd0244458
totakurasivanaga <totakurasivanaga@gmail.com>
2020-03-05 21:29:36 +0530

    useEffectWithClearExample

--- hooks/src/Hooks/HookCounterOne.js
+++ hooks/src/Hooks/HookCounterOne.js
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from 'react';
-
+// useEffect = ComponentDidMount() + ComponentDidUpdate() + ComponentWillUnMount()
  
 const HookCounterOne = () => {
 
     const [count, setCount] = useState(0)
 
+    const [name, setName] = useState('')
+
     useEffect(() => {
+     console.log('useEffect- Updating Document title' )
       document.title = `you clicked ${count} times` 
-    })
+    }, [count])
     return (
         <div>
+            <input type = "text" value = {name} onChange = {e => setName(e.target.value)}/>
            <button onClick = {() => setCount(count + 1)}>{count} times</button> 
         </div>
     );
