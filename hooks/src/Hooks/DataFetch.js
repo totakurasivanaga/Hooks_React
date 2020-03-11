bdfffd99dd40546cb6b06ad095e1ff94914b73d0
totakurasivanaga <totakurasivanaga@gmail.com>
2020-03-11 19:56:57 +0530

    adding minor documentation

--- hooks/src/Hooks/DataFetch.js
+++ hooks/src/Hooks/DataFetch.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import  axios from "axios";
 
+
+// defetch
 const DataFetch = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get("https://jsonplaceholder1.typicode.com/posts1")
       .then(res => {
         console.log(res);
         setPosts(res.data);
